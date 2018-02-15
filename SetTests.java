760568f600e320fc2d4f824967c28b09454296e4
annabush092 <annabush092@gmail.com>
2018-02-15 13:32:11 -0500

    stopping for now

--- SetTests.java
+++ SetTests.java
@@ -10,8 +10,7 @@ public class SetTests {
 
     //run tests
     instantiatesEmptyClass(emptySet);
-    isClassEmpty(emptySet);
-    isClassNotEmpty(oneSet);
+    isClassEmpty(emptySet, oneSet);
   }
 
 // repeatable console responses
@@ -32,22 +31,23 @@ public class SetTests {
     }
   }
 
-  private static void isClassEmpty(MySet emptySet) {
+  private static void isClassEmpty(MySet emptySet, MySet oneSet) {
     System.out.println("Does a class know if it is empty?");
+
+    System.out.println("Empty class is confirmed as empty:");
     if(emptySet.isEmpty() == true) {
       printTrue();
     } else {
       printFalse();
     }
-  }
 
-  private static void isClassNotEmpty(MySet oneSet) {
-    System.out.println("Does a class know if it is not empty?");
+    System.out.println("Not empty class is confirmed as not empty:");
     if(oneSet.isEmpty() == false) {
       printTrue();
     } else {
       printFalse();
     }
+
   }
 
 }
